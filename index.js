f6e5139abb5d396af5b30459e600d32fbd10e519
khamruddin <38712160+khamruddin@users.noreply.github.com>
2018-05-11 10:44:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('!Hello World from khamru!');
+  res.send('!Hello World from psuman!');
 });
 
 var server = app.listen(3000, function () {
