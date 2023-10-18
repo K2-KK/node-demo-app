584af380dda22910b5e2d9868fcac81955a31dfc
KK <38712160+K2-KK@users.noreply.github.com>
2023-10-18 13:34:27 -0500

    Update deleteSnapshot.ps1

--- deleteSnapshot.ps1
+++ deleteSnapshot.ps1
@@ -2,20 +2,16 @@
 $subscriptionId = "d81e9b22-68b1-4ea6-b08f-c3d8a831d67d"
 $resourceGroupName = "testkham"
 
+Param($SnapshotName, $ACIResourceGroup)
+
 # Sign in to Azure (you may need to provide credentials)
 Connect-AzAccount
 
 # Set the subscription context
 Set-AzContext -SubscriptionId $subscriptionId
 
-# Define the snapshot names to delete (passed as a parameter)
-$snapshotNamesToDelete = $env:DEPLOYMENT_SCRIPT_INPUT_SNAPSHOT_NAMES -split ','
-
-# Loop through the snapshot names and delete them
-foreach ($snapshotName in $snapshotNamesToDelete) {
-    Write-Output "Deleting snapshot $snapshotName..."
-    Remove-AzSnapshot -ResourceGroupName $resourceGroupName -SnapshotName $snapshotName -Force
-    Write-Output "Snapshot $snapshotName deleted."
-}
+Write-Output "Deleting snapshot $snapshotName..."
+Remove-AzSnapshot -ResourceGroupName $ACIResourceGroup -SnapshotName $SnapshotName -Force
+Write-Output "Snapshot $snapshotName deleted."
 
 Write-Output "Snapshot cleanup completed."
