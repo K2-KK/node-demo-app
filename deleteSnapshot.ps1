8e07d2401c9930e344d9b196a1c7cd3decbd4818
KK <38712160+K2-KK@users.noreply.github.com>
2023-10-19 23:16:37 -0500

    Update deleteSnapshot.ps1

--- deleteSnapshot.ps1
+++ deleteSnapshot.ps1
@@ -1,4 +1,4 @@
-param($SnapshotName, $ACIResourceGroup)
+param($SnapshotName, $ResourceGroupName)
 
 # Define your Azure subscription and resource group
 $subscriptionId = "d81e9b22-68b1-4ea6-b08f-c3d8a831d67d"
@@ -10,7 +10,7 @@ Connect-AzAccount
 Set-AzContext -SubscriptionId $subscriptionId
 
 Write-Output "Deleting snapshot $snapshotName..."
-Remove-AzSnapshot -ResourceGroupName $ACIResourceGroup -SnapshotName $SnapshotName -Force
+Remove-AzSnapshot -ResourceGroupName $ResourceGroupName -SnapshotName $SnapshotName -Force
 Write-Output "Snapshot $snapshotName deleted."
 
 Write-Output "Snapshot cleanup completed."
